b9ee767394970d2a5738a3ac2d7f5ae583682410
Juan Alvarez <jualvarez@claro.com.ar>
2022-08-26 16:18:32 -0300

    modelos y base creada

--- AppEntregaInt/models.py
+++ AppEntregaInt/models.py
@@ -5,4 +5,12 @@ class equipo(models.Model):
     nombre=models.CharField(max_length=100)
     nombreapp=models.CharField(max_length=100)
 
+class unittest(models.Model):
+    coverage=models.IntegerField()
+    cantidadUT=models.IntegerField()
+
+class regressiontest(models.Model):
+    cantidadRT=models.IntegerField()
+
     
+  

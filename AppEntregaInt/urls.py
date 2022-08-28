c6f510d2bd4fe6efbc1c1d69eb0f723f7428e116
Juan Alvarez <jualvarez@claro.com.ar>
2022-08-28 17:13:55 -0300

    formulario basico funciona

--- AppEntregaInt/urls.py
+++ AppEntregaInt/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('equipo', views.equipo, name="equipo"),
     path('unittest', views.unittest, name="unittest"),
     path('regressiontest', views.regressiontest, name="regressiontest"),
+    path('equipoFormulario', views.equipoFormulario, name="EquipoFormulario"),
 ]
--- AppEntregaInt/views.py
+++ AppEntregaInt/views.py
@@ -3,16 +3,27 @@ from django.http import HttpResponse
 from django.template import loader
 from django.template import Template, Context
 import datetime
+from AppEntregaInt.models import equipo
+#from django.http.request import QueryDict
 
 # Create your views here.
+"""def equipo(request):
+
+      Equipo =  equipo(nombre="pp", nombreapp="sdsd")
+      Equipo.save()
+      documentoDeTexto = f"--->Curso: {equipo.nombre}   Camada: {equipo.nombreapp}"
+
+
+      return HttpResponse(documentoDeTexto)"""
+
 
 def inicio(request):
     #return HttpResponse('vista inicio')
     return render(request, "AppEntregaInt/inicio.html")
 
-def equipo(request):
+#def equipo(request):
     #return HttpResponse('vista equipo')    
-    return render(request, "AppEntregaInt/equipo.html")
+    #return render(request, "AppEntregaInt/equipo.html")
 
 def unittest(request):
     #return HttpResponse('vista unittest')  
@@ -21,3 +32,39 @@ def unittest(request):
 def regressiontest(request):
     #return HttpResponse('vista regressiontest')  
     return render(request, "AppEntregaInt/regressiontest.html") 
+
+#def equipoFormulario(request):
+     # return render(request, "AppEntregaInt/equipoFormulario.html")
+
+
+#def equipoFormulario(request):
+ #     if request.method == 'POST':
+  #    
+   #         equipo =  equipo(request.post['nombre'],(request.post['aplicacion']))
+#
+ #           equipo.save()
+#
+ #           return render(request, "AppEntregaInt/inicio.html")
+#
+ #     return render(request,"AppEntregaInt/equipoFormulario.html") 
+
+
+
+from AppEntregaInt.forms import EquipoFormulario
+
+def equipoFormulario(request):
+
+      if request.method == "POST":
+
+            miFormulario = EquipoFormulario(request.POST) # Aqui me llega la informacion del html
+            print(miFormulario)
+
+            if miFormulario.is_valid:
+                  informacion = miFormulario.cleaned_data
+                  vequipo = equipo(nombre=informacion["nombre"], nombreapp=informacion["aplicacion"])
+                  vequipo.save()
+                  return render(request, "AppEntregaInt/inicio.html")
+      else:
+            miFormulario = EquipoFormulario()
+
+      return render(request, "AppEntregaInt/equipoFormulario.html", {"miFormulario": miFormulario})      

a894dbc9ed2b923d0634265d9b08407b790cf393
Juan Alvarez <jualvarez@claro.com.ar>
2022-08-29 08:54:24 -0300

    segundo formulario funcionando

---
+++ AppEntregaInt/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class EquipoFormulario(forms.Form): # JM - creo una clase esto es lo que recibe la vista
+    nombre = forms.CharField(max_length=100)
+    aplicacion = forms.CharField(max_length=100)
+
+#creo la clase para el unittest
+class UnittestFormulario(forms.Form):
+    coverage = forms.IntegerField()
+    cantidadUT = forms.IntegerField()
--- AppEntregaInt/urls.py
+++ AppEntregaInt/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('equipo', views.equipo, name="equipo"),
     path('unittest', views.unittest, name="unittest"),
     path('regressiontest', views.regressiontest, name="regressiontest"),
+    path('equipoFormulario', views.equipoFormulario, name="EquipoFormulario"), # primera clase form
+    path('busquedaAplicacion',  views.busquedaAplicacion, name="BusquedaAplicacion"),#agrego busqueda
+    path('buscar/', views.buscar), #busqueda
+    path('unittestFormulario', views.unittestFormulario, name="unittestFormulario"),#segunda clase form
+
 ]
